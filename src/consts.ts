7f692644690501d949256812e8b758521d142a59
TKaku <kaku101127@gmail.com>
2026-06-15 18:33:09 +0900

    feat: add JSON-LD structured data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Meiscope Blog';
+export const SITE_DESCRIPTION = 'AIに読まれるWebサイトの作り方を実測・記録するブログ。';
+export const SITE_AUTHOR = 'Meiscope';
